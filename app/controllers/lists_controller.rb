2c96038b12d13dd491310e362f4b093ba57a5bdb
tanikura <122531271+tanikura@users.noreply.github.com>
2023-02-13 11:49:19 +0000

    [Finish] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -36,7 +36,7 @@ class ListsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
 
